a52a23e1ed101eea8aa4095abfe6424439d38517
MrHenger <henry.r0701@gmail.com>
2022-01-29 16:50:01 -0400

    se agrego la dependencia axios

--- src/main.js
+++ src/main.js
@@ -4,8 +4,10 @@ import router from './router';
 import store from './store';
 import vuetify from './plugins/vuetify';
 import './vee-validate';
+import Axios from 'axios';
 
 Vue.config.productionTip = false;
+Vue.prototype.$axios = Axios;
 
 new Vue({
   router,
