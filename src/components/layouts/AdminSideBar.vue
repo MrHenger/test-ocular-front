8567deb6cb0b52f17e02058c8aef42024c26dfe4
MrHenger <henry.r0701@gmail.com>
2022-01-29 22:55:44 -0400

    feat: se creo el componente sideBar para las vistas del admin

---
+++ src/components/layouts/AdminSideBar.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="sb-background" style="height: 100%">
+    <v-list dark dense class="sb-background">
+      <v-list-item-group v-model="selectedItem" color="primary">
+        <v-list-item v-for="(item, i) in items" :key="i">
+          <v-list-item-icon>
+            <v-icon v-text="item.icon"></v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.text"></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      selectedItem: 0,
+      items: [
+        { text: 'Publicaciones', icon: 'mdi-post' },
+        { text: 'Categorias', icon: 'mdi-server' },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+.sb-background {
+  background-color: #1e1e1e !important;
+}
+</style>
