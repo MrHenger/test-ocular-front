b23ef39adb1c9c2fcd5af439352ab3bf219353f8
MrHenger <henry.r0701@gmail.com>
2022-01-29 21:53:37 -0400

    feat: se creo una instancia global de axios
    esta contiene la configuracion basica para poder consultar la apii

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from './router';
 import store from './store';
 import vuetify from './plugins/vuetify';
 import './vee-validate';
-import Axios from 'axios';
+import Axios from './axios';
 
 Vue.config.productionTip = false;
 Vue.prototype.$axios = Axios;
