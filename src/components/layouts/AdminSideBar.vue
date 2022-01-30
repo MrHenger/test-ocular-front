760f3e1acd8f70b68d19f34df28ae6c6cf09efac
MrHenger <henry.r0701@gmail.com>
2022-01-30 14:06:16 -0400

    feat: se agrego la informacion del usuario en el sidebar

--- src/components/layouts/AdminSideBar.vue
+++ src/components/layouts/AdminSideBar.vue
@@ -1,21 +1,46 @@
 <template>
   <div class="sb-background" style="height: 100%">
-    <v-list dark dense class="sb-background">
-      <v-list-item-group v-model="selectedItem" color="primary">
-        <v-list-item v-for="(item, i) in items" :key="i">
-          <v-list-item-icon>
-            <v-icon v-text="item.icon"></v-icon>
-          </v-list-item-icon>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.text"></v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list-item-group>
-    </v-list>
+    <div>
+      <div class="pt-10 pb-4">
+        <div class="d-flex justify-center">
+          <v-avatar size="128">
+            <v-img :src="authme.fullpath" max-width="200" max-height="200"></v-img>
+          </v-avatar>
+        </div>
+        <div class="mt-4">
+          <v-container>
+            <v-row>
+              <v-col cols="12" class="d-flex justify-center py-1">
+                <span class="white--text">{{ authme.name }}</span>
+              </v-col>
+              <v-col cols="12" class="d-flex justify-center py-0">
+                <span class="white--text">{{ authme.email }}</span>
+              </v-col>
+            </v-row>
+          </v-container>
+        </div>
+      </div>
+      <v-divider dark class="mx-8"></v-divider>
+      <div>
+        <v-list dark dense class="sb-background">
+          <v-list-item-group v-model="selectedItem" color="primary">
+            <v-list-item v-for="(item, i) in items" :key="i">
+              <v-list-item-icon>
+                <v-icon v-text="item.icon"></v-icon>
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title v-text="item.text"></v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import { mapActions, mapState } from 'vuex';
 export default {
   data() {
     return {
@@ -25,6 +50,15 @@ export default {
         { text: 'Categorias', icon: 'mdi-server' },
       ],
     };
+  },
+  created() {
+    this.getAuthme();
+  },
+  computed: {
+    ...mapState(['authme']),
+  },
+  methods: {
+    ...mapActions(['getAuthme']),
   },
 };
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -3,11 +3,32 @@ import Vuex from 'vuex';
 import post from './post';
 
 Vue.use(Vuex);
+import axios from '../axios';
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    authme: {},
+  },
+  mutations: {
+    setAuthme(state, data) {
+      state.authme = data;
+    },
+  },
+  actions: {
+    getAuthme(contex) {
+      axios
+        .get('/authme')
+        .then((res) => {
+          if (res.status == 200) {
+            console.log(res.data);
+            contex.commit('setAuthme', res.data);
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
   modules: {
     post,
   },
